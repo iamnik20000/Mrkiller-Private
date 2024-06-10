0daeab4294cb6b2d1703a6018c4c71888fe810f6
iamnik20000 <nikesh25024@gmail.com>
2024-06-10 19:41:53 +0530

    MrkillerDeveloper

---
+++ server/__init__.py
@@ -0,0 +1,2 @@
+
+# I have my dirty mind @LazyDeveloperr & @MrkillerDeveloper 😎😍
---
+++ server/exceptions.py
@@ -0,0 +1,10 @@
+
+    # 🥰  Thank you for helping  this journey  @LazyDeveloperr  🥰
+    # thanks Adarsh Goel for Server Routing
+    #thanks @MrkillerDeveloper 
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
